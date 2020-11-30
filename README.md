12d13238b30ad2d1325d1e4a845fd21c6123e856
Int <int@int.int>
2020-11-30 11:40:31 +0300

    Added gitignore

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Clock.loc
+# Uploaded via GIT
--- index.php
+++ index.php
@@ -4,12 +4,6 @@ session_start();
 
 define("ROOT", dirname(__FILE__));
 
-
-// ini_set('error_reporting', E_ALL);
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-
-
 function debug($value)
 {
 
@@ -36,7 +30,6 @@ function debug($value)
 
 
 require_once(ROOT.'/vendor/autoload.php');
-
 
 $db = \DB\DB::Connect();
 
